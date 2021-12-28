318206cf9ea0dae44eba4ae12480d0ff88c008a6
ander7even <ander7even@mail.ru>
2021-12-28 21:09:51 +0900

    11

---
+++ Task_14/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+            cat.ShowInfo();
+            dog.ShowInfo();
+
+            Console.ReadKey();
+
+        }
+    }
+
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public Animal()
+        {
+
+        }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+
+    }
+    class Cat : Animal
+    {
+        string name = "Кошка Мурка";
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+    class Dog : Animal
+    {
+        string name = "Собака Шарик";
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+    }
+
+
+
+}
